d608b1262dbd9ad140ae1e91c26d38942be26210
filipbe <filipbe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-03-21 21:36:11 +0000

    0.7.4 Event products list styling
    git-svn-id: https://plugins.svn.wordpress.org/balfolk-tickets/trunk@1844464 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/js/event-tickets.js
+++ public/js/event-tickets.js
@@ -1,3 +1,12 @@
-function xx() {
-	
-}
+jQuery(document).ready(function($){
+	$(".prod-qty input").change(function() {
+		$total = $(this)
+			.closest('form.cart')
+			.find('.prod-total p.bft-total');
+		
+		$total.text(
+				($(this).val() * $total.data("price")) 
+				+ " " + $total.data("symbol")
+		);
+	});
+});
